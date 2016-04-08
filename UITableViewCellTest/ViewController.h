9b844027a2ad6ba73e28f697a6c8adee19144e83
Gareth Bestor <bestor@gareths-macbook-pro.fritz.box>
2016-04-08 12:40:31 +1200

    Initial Commit

---
+++ UITableViewCellTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewCellTest
+//
+//  Created by Gareth Bestor on 8/04/16.
+//  Copyright © 2016 Xiphware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewCellTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITableViewCellTest
+//
+//  Created by Gareth Bestor on 8/04/16.
+//  Copyright © 2016 Xiphware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
